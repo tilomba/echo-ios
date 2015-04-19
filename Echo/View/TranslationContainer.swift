bb69b6df2983d156d558b8d9b4c6bc99c0426502
Douglas Bumby <douglas.bumby@icloud.com>
2015-04-19 02:13:25 -0400

    Structural change

---
+++ Echo/View/TranslationContainer.swift
@@ -0,0 +1,21 @@
+//
+//  TranslationContainer.swift
+//  Echo
+//
+//  Created by Douglas Bumby on 2015-04-19.
+//  Copyright (c) 2015 Cosmic Labs. All rights reserved.
+//
+
+import UIKit
+
+class TranslationContainer: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
