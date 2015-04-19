1de3c4ef50a066d8f26633fe7868ee6a32b30304
Damon Jones <damon@damonjones.me>
2015-04-19 00:07:08 -0400

    Add translation class and textview

---
+++ Echo/NATODictionary.swift
@@ -0,0 +1,59 @@
+//
+//  NATODictionary.swift
+//  Echo
+//
+//  Created by Damon Jones on 4/18/15.
+//  Copyright (c) 2015 Cosmic Labs. All rights reserved.
+//
+
+import Foundation
+
+class NATODictionary {
+    
+    static let dictionary = [
+        "A": "ALPHA",
+        "B": "BRAVO",
+        "C": "CHARLIE",
+        "D": "DELTA",
+        "E": "ECHO",
+        "F": "FOXTROT",
+        "G": "GOLF",
+        "H": "HOTEL",
+        "I": "INDIA",
+        "J": "JULIET",
+        "K": "KILO",
+        "L": "LIMA",
+        "M": "MIKE",
+        "N": "NOVEMBER",
+        "O": "OSCAR",
+        "P": "PAPA",
+        "Q": "QUEBEC",
+        "R": "ROMEO",
+        "S": "SIERRA",
+        "T": "TANGO",
+        "U": "UNIFORM",
+        "V": "VICTOR",
+        "W": "WHISKEY",
+        "X": "X-RAY",
+        "Y": "YANKEE",
+        "Z": "ZULU",
+        "0": "ZERO",
+        "1": "ONE",
+        "2": "TWO",
+        "3": "THREE",
+        "4": "FOUR",
+        "5": "FIVE",
+        "6": "SIX",
+        "7": "SEVEN",
+        "8": "EIGHT",
+        "9": "NINE"
+    ]
+    
+    static func translateCharacter(character: String) -> String? {
+        if let translatedCharacter = dictionary[character.uppercaseString] {
+            return translatedCharacter
+        }
+        
+        return nil
+    }
+}
--- Echo/ViewController.swift
+++ Echo/ViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if let translation = NATODictionary.translateCharacter(text as String) {
+            textView.text = textView.text + translation + " "
+
+            return false
+        }
+        
+        return true
     }
-
-
 }
 
