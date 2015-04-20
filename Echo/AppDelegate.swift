a5cedbb5f299a3f1a3db742395161e39de84e511
Damon Jones <damon@damonjones.me>
2015-04-20 00:14:20 -0400

    Add corner radius to UIWindow

--- Echo/AppDelegate.swift
+++ Echo/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window?.layer.cornerRadius = 5
+        window?.layer.masksToBounds = true
+        
         return true
     }
 
