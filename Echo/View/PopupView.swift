d969a29631d3225cbf2c109595f4c045ede30177
Douglas Bumby <douglas.bumby@icloud.com>
2015-04-20 16:14:06 -0400

    Refactor constants and change font style

--- Echo/View/PopupView.swift
+++ Echo/View/PopupView.swift
@@ -11,7 +11,8 @@ import UIKit
 @IBDesignable
 public class PopupView: UIView {
     struct Constants {
-        static var radius = CGFloat(200)
+        static let radius = CGFloat(6)
+        static let fontSize = CGFloat(17.0)
     }
     
     override public func prepareForInterfaceBuilder() {
@@ -28,11 +29,36 @@ public class PopupView: UIView {
     }
     
     private func setup() {
-        layer.cornerRadius = 6
+        layer.cornerRadius = Constants.radius
         layer.masksToBounds = true
         let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
         blurView.bounds = bounds
         blurView.center = center
         addSubview(blurView)
+        
+        buttons()
+    }
+    
+    private func buttons() {
+        let pasteButton = UIButton.buttonWithType(.Custom) as! UIButton
+        pasteButton.setTitle("paste", forState: .Normal)
+        pasteButton.setTitleColor(UIColor.lightThemeContainerColor(), forState: .Normal)
+        pasteButton.frame = CGRect(x: 0.0, y: 0.0, width: 99.66, height: 93.0)
+        pasteButton.titleLabel!.font =  UIFont.helveticaNeueLight(Constants.fontSize)
+        addSubview(pasteButton)
+        
+        let clearButton = UIButton.buttonWithType(.Custom) as! UIButton
+        clearButton.setTitle("clear", forState: .Normal)
+        clearButton.setTitleColor(UIColor.lightThemeContainerColor(), forState: .Normal)
+        clearButton.frame = CGRect(x: 99.66, y: 0.0, width: 99.66, height: 93.0)
+        clearButton.titleLabel!.font =  UIFont.helveticaNeueLight(Constants.fontSize)
+        addSubview(clearButton)
+        
+        let copyButton = UIButton.buttonWithType(.Custom) as! UIButton
+        copyButton.setTitle("copy", forState: .Normal)
+        copyButton.setTitleColor(UIColor.lightThemeContainerColor(), forState: .Normal)
+        copyButton.frame = CGRect(x: 199.32, y: 0.0, width: 99.66, height: 93.0)
+        copyButton.titleLabel!.font =  UIFont.helveticaNeueLight(Constants.fontSize)
+        addSubview(copyButton)
     }
 }
